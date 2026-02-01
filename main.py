34e7c11b0e25f71d9781ac41394c2c8c16b1169c
gorisha3730beai25-cyber <gorisha3730.beai25@chitkara.edu.in>
2026-02-01 15:58:38 +0530

    Update and rename main.py.txt to main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 print("Welcome to student record management system")
 
 name = input("Enter the name:")
-marks = int(input("Enter the marks:")
+marks = int(input("Enter the marks:"))
 
 if marks >=50:
     print(f"{name} has passed with {marks} marks.")
 else:
     print(f"{name} has failed with {marks} marks.")
+
 
