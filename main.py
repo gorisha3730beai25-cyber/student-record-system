c6f19e48913d3058eb66e02666094e769281e086
gorisha3730beai25-cyber <gorisha3730.beai25@chitkara.edu.in>
2026-02-01 16:17:18 +0530

    integrated Student class in main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+from student import Student
 print("Welcome to student record management system")
-
 name = input("Enter the name:")
 marks = int(input("Enter the marks:"))
-
-if marks >=50:
-    print(f"{name} has passed with {marks} marks.")
-else:
-    print(f"{name} has failed with {marks} marks.")
-
+student = Student(name,marks)
+result = student.result()
+print(f"{name} has {result} with {marks} marks.")
